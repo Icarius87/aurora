4f987506c39977f832af28bf8f4ca57c40576402
Icarius87 <41232149+Icarius87@users.noreply.github.com>
2020-01-02 14:41:17 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-function atload() {dom_rdy()}window.onload=atload;
+// function atload() {dom_rdy()}window.onload=atload;
 
 const hamburger = document.querySelector(".hamburger");
 const nav = document.querySelector("nav");
@@ -9,3 +9,15 @@ nav.classList.toggle("on");
 
 })
 
+var fotka = document.getElementsByClassName("galeryPictures");
+function scalePicture(){
+    this.classList.toggle('picUp');
+}
+var i;
+function powFot(){
+    for(i = 0; i < fotka.length; i++) {
+        fotka[i].addEventListener('click',scalePicture);
+      } 
+}
+
+powFot();
